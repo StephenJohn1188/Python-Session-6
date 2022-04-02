8a9efd9c861109c07c3d34d3484f3bd93ce3ea22
Santhosh Kumar Mahalingam <99348330+StephenJohn1188@users.noreply.github.com>
2022-04-02 20:59:02 +0530

    Add files via upload

---
+++ Python Session 6 if Construct.py
@@ -0,0 +1,337 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Else If Condition
+
+# In[7]:
+
+
+Marks=58
+
+
+# In[13]:
+
+
+if Marks>50:
+    print('Pass')
+else:
+     print('Fail')
+
+
+# In[14]:
+
+
+Marks=40
+if Marks>50:
+    print('Pass')
+else:
+     print('Fail')
+       
+
+
+# In[15]:
+
+
+x=159
+if x>100:
+    print("X is Greater than 100")
+else:
+    print("X is Lesser than 100")
+
+
+# In[16]:
+
+
+x=88
+if x>100:
+    print("X is Greater than 100")
+else:
+    print("X is Lesser than 100")
+
+
+# In[17]:
+
+
+x=10
+if x%2==0:
+    print('The given number is even')
+else:
+    print('The given number is odd')
+
+
+# In[18]:
+
+
+p=int(input("Enter the Number"))
+if p%2==0:
+    print('The number is even')
+else:
+    print('The number is odd')
+
+
+# In[24]:
+
+
+Marks=int(input('Enter the Marks'))
+if Marks>50:
+    print('The Student has passed the exam')
+else:
+    print('The Student has failed the exam')
+
+
+# In[25]:
+
+
+List=[1,2,3,4,5]
+
+
+# In[26]:
+
+
+List
+
+
+# In[30]:
+
+
+for List in [1,2,3,4,5]:
+    if List==3:
+        break
+    print(List)
+
+
+# Break Statement
+
+# In[31]:
+
+
+for i in ['Mango','Banana','Papaya','Orange','Jackfruit']:
+    if i=='Orange':
+        break
+    print(i)
+
+
+# In[32]:
+
+
+for k in [10,20,30,40,50]:
+    print(k)
+    print("End")
+print("End of loop")
+
+
+# In[35]:
+
+
+for i in[1,2,3,4,5]:
+    if i==4:
+        continue
+    print(i)
+
+
+# In[40]:
+
+
+for j in[10,20,30,40,50,60,70,80,90]:
+    if j==40:
+        continue
+    print(j)
+    print("END")
+print("End of Continue")
+
+
+# LIST COMPREHENSION
+
+# In[47]:
+
+
+List1=[1,2,3,4,5,6,7,8,9]
+p=[p for p in List1 if p%2==0]
+p
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[44]:
+
+
+p=[p for p in List1 if p%2==0]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
